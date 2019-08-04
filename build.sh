3e6fb9c9ccaa57762e2bafb6724b51cc5f039f75
sillyhat <xushikuan1@163.com>
2019-08-04 12:22:18 +0800

    add docker clear

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 go clean
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o main main.go
 docker build -t xushikuan/temp-backend .
-docker push xushikuan/temp-backend:1.6
+docker tag xushikuan/temp-backend:latest xushikuan/temp-backend:1.7
+docker push xushikuan/temp-backend:1.7
 go clean
